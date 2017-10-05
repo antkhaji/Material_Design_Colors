41bc94d1b7c8e2992242b5f5fab686f4f6ad1e37
Anthony Bukhalana <antkhaji@gmail.com>
2017-10-05 15:31:09 +0300

    Update Recipe.java

--- Recipe.java
+++ Recipe.java
@@ -70,18 +70,3 @@ public class Recipe {
 
 
 
-
-
-        mListView = (ListView) findViewById(R.id.recipe_list_view);
-// 1
-        final ArrayList<Recipe> recipeList = Recipe.getRecipesFromFile("recipes.json", this);
-// 2
-        String[] listItems = new String[recipeList.size()];
-// 3
-        for(int i = 0; i < recipeList.size(); i++){
-            Recipe recipe = recipeList.get(i);
-            listItems[i] = recipe.title;
-        }
-// 4
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
-        mListView.setAdapter(adapter);
